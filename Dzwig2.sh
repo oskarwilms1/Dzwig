bef53b0a742311567b2401b7191a9e24a5f869ac
oskarwilms1 <149292696+oskarwilms1@users.noreply.github.com>
2024-12-06 10:39:41 +0100

    Update Dzwig2.sh

--- Dzwig2.sh
+++ Dzwig2.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
-
 echo "Dźwig 2 zaczyna pracę..."
 przeniesione=0
+dzwig1_pid=$1
+
+# Funkcja do obsługi sygnałów
+handle_dzwig1_finished() {
+    echo "Dźwig 1 zakończył pracę, dźwig 2 kończy prace."
+    exit 0
+}
+
+# Podpięcie funkcji do obsługi sygnału
+trap handle_dzwig1_finished USR1
 
 while true; do
-    if [ "$(ls -1 Bufor | wc -l)" -gt 0 ]; then
+    # Sprawdzanie, czy są pliki w buforze
+    if [ "$(ls -1 Bufor 2>/dev/null | wc -l)" -gt 0 ]; then
         file=$(ls -1 Bufor | head -n 1)
         mv "Bufor/$file" "Miejsce2/$file"
         echo "Dźwig 2 przenosi: $file"
         ((przeniesione++))
         sleep 1  # symulacja czasu przenoszenia
     else
-        # Zakończ operacje, jeśli dźwig 1 skończył i bufor jest pusty
-        if ! ps -p "$dzwig1_pid" > /dev/null; then
-            break
-        fi
-    fi
+        # Jeśli nie ma plików w buforze, sprawdzamy ponownie
+        sleep 1
+    done
 done
 
 echo "Dźwig 2 zakończył pracę."
