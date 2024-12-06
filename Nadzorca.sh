c2356a39d8da1f4618b1258e554c7c5d0cc6e7a5
oskarwilms1 <149292696+oskarwilms1@users.noreply.github.com>
2024-12-06 10:39:07 +0100

    Update Nadzorca.sh

--- Nadzorca.sh
+++ Nadzorca.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+# Inicjalizacja zmiennych
+dzwig1_pid=0
+dzwig2_pid=0
+ilosc_przeniesionych_1=0
+ilosc_przeniesionych_2=0
+
+# Funkcje na sygnały
 handle_usr1() {
     echo "Zaczynam pracę dźwigów..."
+
+    # Usunięcie pliku finalizacji dźwigu 1, jeśli istnieje
+    rm -f "dzwig1_finished"
 
     # Uruchomienie dźwigów w tle
     ./dzwig1.sh &
     dzwig1_pid=$!
-    ./dzwig2.sh &
+    ./dzwig2.sh "$dzwig1_pid" &
     dzwig2_pid=$!
 
     echo "Dźwigi uruchomione: PID1=$dzwig1_pid PID2=$dzwig2_pid"
@@ -14,17 +24,17 @@ handle_usr1() {
 
 handle_sigint() {
     echo "Otrzymano sygnał SIGINT, kończenie pracy..."
-    
+
     # Zatrzymanie dźwigów
     kill $dzwig1_pid
     kill $dzwig2_pid
-    
+
     # Wyświetlenie rezultatów
     wait $dzwig1_pid
     ilosc_przeniesionych_1=$?
     wait $dzwig2_pid
     ilosc_przeniesionych_2=$?
-    
+
     echo "Dźwig 1 przeniósł $ilosc_przeniesionych_1 materiałów."
     echo "Dźwig 2 przeniósł $ilosc_przeniesionych_2 materiałów."
     
@@ -40,4 +50,10 @@ echo "Nadzorca uruchomiony. Czekam na sygnał..."
 # W nieskończonej pętli
 while true; do
     sleep 1
+    # Sprawdzamy, czy dźwig 1 zakończył pracę
+    if ! ps -p "$dzwig1_pid" > /dev/null 2>&1; then
+        touch "dzwig1_finished"
+        echo "Dźwig 1 zakończył pracę. Informuję dźwig 2."
+        kill -USR1 "$dzwig2_pid"  # Informowanie dźwigu 2
+    fi
 done
